b1e5b3a0b9df82e347e90061a6dfa60d7c3cec6d
Anthony Leif <tonyleif@hotmail.com>
2018-04-23 23:12:22 -0500

    Added routing, contact page, and empty about page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { CommonModule } from '@angular/common';
-
 import { AppComponent } from './app.component';
 import { RegularSeasonGames2017Service } from './model/regular-season-games-2017.service';
 import { RegularSeasonPlays2017Service } from './model/regular-season-plays-2017.service';
 import { RegularSeasonActivePlayers2017Service } from './model/regular-season-active-players-2017.service';
 import { TeamService } from './model/team.service';
 import { GamesComponent } from './components/games/games.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { Route } from '@angular/compiler/src/core';
+import { RouterLinkActive } from '@angular/router';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/games', pathMatch: 'full'},
+  {path: 'games',   component: GamesComponent },
+  {path: 'contact', component: ContactComponent },
+  {path: 'about',   component: AboutComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    GamesComponent
+    GamesComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ], // CommonModule
   providers: [RegularSeasonGames2017Service, RegularSeasonPlays2017Service, RegularSeasonActivePlayers2017Service, TeamService],
   bootstrap: [AppComponent]
